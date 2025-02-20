6febd8cd7bba2c34b00075a753ee700e7d6d01f0
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-21 00:15:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function stringChop(str, size) {
-  // your code here
-	if (!str) return []; 
+    if (!str) return [];
+    size = parseInt(size, 10);
+
+    if (isNaN(size) || size <= 0) return [];
 
     let result = [];
-    for (let i = 0; i < str.length; i += chunkSize) {
-        result.push(str.substring(i, i + chunkSize)); 
+    for (let i = 0; i < str.length; i += size) {
+        result.push(str.substring(i, i + size));
     }
     return result;
 }
 
-// Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+alert(JSON.stringify(stringChop(str, size)));
