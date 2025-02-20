25f9398196dfda24b2116c6491694d4be19520de
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-21 00:19:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function stringChop(str, size) {
-    if (!str) return [];
+    if (!str) return "";
     size = parseInt(size, 10);
 
-    if (isNaN(size) || size <= 0) return [];
+    if (isNaN(size) || size <= 0) return "";
 
     let result = [];
     for (let i = 0; i < str.length; i += size) {
         result.push(str.substring(i, i + size));
     }
-    return result;
+    return result.join(","); 
 }
 
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(JSON.stringify(stringChop(str, size)));
+alert(stringChop(str, size));
