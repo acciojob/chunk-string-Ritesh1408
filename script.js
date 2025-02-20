607dc2a0d83b325fd54799d614a01bad8d44b6d2
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-21 00:11:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
   // your code here
+	if (!str) return []; 
+
+    let result = [];
+    for (let i = 0; i < str.length; i += chunkSize) {
+        result.push(str.substring(i, i + chunkSize)); 
+    }
+    return result;
 }
 
 // Do not change the code below
